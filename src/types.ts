89594f80f1f1cf4c8177f85eaa00e9b183dbb104
WTomas <wtomas98@gmail.com>
2024-10-16 14:40:42 +0100

    fix: allow null | undefined return type for transform + fix README

--- src/types.ts
+++ src/types.ts
@@ -8,11 +8,17 @@ export type CSVColumnOptions<T extends CSVTemplate> = Partial<{
   [K in keyof T]: ColumnOptions<T, K>;
 }>;
 
+export type CSVColumnTransform<T extends CSVTemplate, K extends keyof T> = (
+  value: T[K],
+  index,
+  values: Array<T[K]>
+) => string | undefined | null;
+
 export type ColumnOptions<T extends CSVTemplate, K extends keyof T> = Partial<{
   priority: number;
   emptyValue: string | null;
   removeIfEmpty: boolean;
-  transform?: (value: T[K], index, values: Array<T[K]>) => string;
+  transform?: CSVColumnTransform<T, K>;
 }>;
 
 export type CSVBuilderOptions = {
